21ef8a0a10e4b6b478000974e437b6b3546a3c9c
voodoobettie <ingrid@frankenmedia.ca>
2016-10-12 13:25:03 -0700

    Added user association to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tasks, dependent: :destroy
+  belongs_to :user
+
 end
